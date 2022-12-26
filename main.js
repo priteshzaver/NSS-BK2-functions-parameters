4863f20166473ea4b7f26642b6c50ee1cb27228f
Pritesh Zaver <priteshzaver@gmail.com>
2022-12-26 13:47:33 -0600

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+const quarterValue = (value) => {
+    const quarter = value / 4
+    return quarter
+}
+
+const returnedValue = quarterValue(4)
+
+console.log(returnedValue)
---
+++ readme.md
@@ -0,0 +1,1 @@
+The purpose of this project is the use of parameters for a function.
